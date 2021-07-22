006b1811f23c6341e8559d6c4b56f9cb89c97dc6
Viktorija Z <25870341+vzaboraite@users.noreply.github.com>
2021-07-22 22:51:02 +0100

    Add pseudo-code

--- src/index.js
+++ src/index.js
@@ -7,4 +7,32 @@
 
 // - Render all the cards on the page that represents all the pokemons, recreating the same layout, using JS
 
-console.log(data);
+// console.log(data);
+
+// 1.0 Create the bridge using <ul class="cards"></ul>
+
+// 2.0 createPokemonCard(pokemon)
+// - create an <h2>
+//    - text comes from pokemon.name
+// - create an <img>
+//    - src comes from sprites.other["official-artwork"].front_default
+// - run createStatsList() and append
+//
+// input: an object that represents a pokemon
+// output: <li> element that looks like the the card template
+//
+// How could I test this on the page rather than a console.log?
+
+// 2.1 createStatsList(stats)
+// - The list of stats
+// - For loop to create each stat from a "stats" array
+//
+// input: an array of stats from a pokemon object
+// output: <ul> element with various <li> elements
+
+// 2.2 renderPokemonCards(data)
+// - A for loop on the array of pokemon
+//   - run createPokemonCard()
+//   - append to the element from Step 1.0
+// - input: an array of objects that represent pokemon (data)
+// - output: -
